5a3db486aa26a6724ab01ac004fa68bba4025e05
tamaki <hbk2002320@gmail.com>
2020-11-13 14:30:25 +0900

    create answerlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
-import './assets/styles/style.css'
+import './assets/styles/style.css';
+import {AnswersList} from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,16 +10,29 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      detaset: {},
+      dataset: defaultDataset,
       open: false
     }
+  }
+
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+
+    this.setState({
+      answers: initAnswers
+    })
+  }
+
+  componentDidMount() {
+    this.initAnswer()
   }
 
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-            
+            <AnswersList answers={this.state.answers}/>
         </div>
       </section>
     );
