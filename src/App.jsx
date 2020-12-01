fdd152e91b79c4d853005681c3285f057070fefb
tamaki <hbk2002320@gmail.com>
2020-12-01 16:43:39 +0900

    conect firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import {AnswersList, Chats} from "./components/index";
 import FormDaialog from './components/Forms/FormDaialog';
+import {db} from './firebase/index';
 
 
 export default class App extends React.Component {
@@ -12,7 +12,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -20,6 +20,9 @@ export default class App extends React.Component {
     this.handleClickOpen = this.handleClickOpen.bind(this)
   }
 
+    /* @nextQuestionId ユーザーの回答を受け取る 
+   * 質問とチャットをstateへセットする
+   */
   displayNextQuestion = (nextQuestionId) => {
     const chats = this.state.chats;
     chats.push({
@@ -34,16 +37,20 @@ export default class App extends React.Component {
     })
   }
 
+  /* ユーザーの回答を受け取り、chatsへ保存 */
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
+       /* 初期値 */
       case (nextQuestionId === 'init'):
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
 
+      /* お問い合わせの場合、モーダルを開く(setStateの変更) */
       case (nextQuestionId === 'contact'):
         this.handleClickOpen();
         break;
 
+      /* URL時 */
       case(/http:*/.test(nextQuestionId)):
         const a = document.createElement('a');
         a.href = nextQuestionId;
@@ -51,7 +58,9 @@ export default class App extends React.Component {
         a.click();
         break;
 
+      /* 回答時 */
       default:
+        // チャットに回答するvalueを「chats」へ入れる
         const chats = this.state.chats;
         chats.push({
           text: selectedAnswer,
@@ -62,6 +71,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
+        // state変更
         setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
     }
@@ -75,11 +85,36 @@ export default class App extends React.Component {
     this.setState({ open: false });
   };
 
+  // データベースから取得後、stateを更新する
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset});
+  };
+
+   /* ライフサイクル */
+  // アンサーが回答される度にselectAnswerメソッドが実行される
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId)
+    // 即時関数を使ってデータベースから取得を行う
+    (async() => {
+      const dataset = this.state.dataset;
+
+      // awaitをすることで中の処理が終えるのを待ってくれる
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id;
+          const data = doc.data();
+          dataset[id] = data;
+        })
+      })
+
+      // state更新
+      this.initDataset(dataset)
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId);
+
+    })();
   }
 
+  // 自動スクロールを実装
   componentDidUpdate() {
     const scrollArea = document.getElementById('scroll-area');
     if (scrollArea) {
